38536e73e0a4d5fa10500032502cc4e0b2d14386
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-19 10:29:50 -0400

    adds data to append

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+var menuItems = [
+    {
+        href: "http://apple.com",
+        text: 'about'
+    },
+    {
+        href: "http://google.com",
+        text: 'contact'
+    },
+    {
+        href: "http://www.digitalcrafts.com",
+        text: 'blog'
+    },
+    {
+        href: "http://www.wikipedia.com",
+        text: 'portfolio'
+    }
+];
